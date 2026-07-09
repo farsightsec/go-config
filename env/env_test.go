37f8e907a6281059d2e2d0aa8d08b3e1a14ca5a9
Rafael Vanoni <rvanoni@domaintools.com>
2026-07-09 07:17:40 -0700

    fix copyright lines in env/*

--- env/env_test.go
+++ env/env_test.go
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2026 DomainTools LLC
- * Copyright 2023 Farsight Security, Inc.
+ * Copyright (c) 2023, 2026 DomainTools LLC
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
