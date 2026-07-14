63127de3dc74a043641dfd508a33d127528b4c41
Rafael Vanoni <rvanoni@domaintools.com>
2026-07-14 08:08:06 -0700

    fix copyrights

--- env/example_test.go
+++ env/example_test.go
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2018, 2026 DomainTools LLC
+ * Copyright 2018 Farsight Security, Inc.
+ * Copyright 2026 DomainTools LLC
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
