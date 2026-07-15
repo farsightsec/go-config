87a0f0f19b8a08b453e30cdf9931f3db9ea0bef3
Allan LeSage <alesage@domaintools.com>
2026-07-15 12:36:21 -0500

    Build fix-ups and copyright nits.

--- env/env.go
+++ env/env.go
@@ -1,6 +1,6 @@
 /*
- * Copyright 2018 Farsight Security, Inc.
  * Copyright 2026 DomainTools LLC
+ * Copyright 2018 Farsight Security, Inc.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
