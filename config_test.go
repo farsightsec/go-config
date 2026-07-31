8891277ea5356cd81dd7b2dc5f5784cb10f9060c
Rafael Vanoni <rvanoni@domaintools.com>
2026-07-31 01:48:21 -0700

    various unit test fixes

--- config_test.go
+++ config_test.go
@@ -1,19 +1,28 @@
+/*
+ * Copyright 2026 DomainTools LLC
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package config
 
 import (
-	"testing"
+	"crypto/tls"
 	"fmt"
 	"os"
-	"io/ioutil"
+	"testing"
 
 	"encoding/json"
+
 	yaml "gopkg.in/yaml.v2"
 )
 
-
 func getTempFile(t *testing.T, ext string, content string) string {
+	t.Helper()
 
-	tmpfile, err := ioutil.TempFile("", "tmpdata." + ext)
+	tmpfile, err := os.CreateTemp("", "tmpdata."+ext)
 	if err != nil {
 		t.Errorf("Error creating temp file of ext %s", ext)
 		return ""
@@ -29,157 +38,178 @@ func getTempFile(t *testing.T, ext string, content string) string {
 	return tmpfile.Name()
 }
 
+func testMarshalling(t *testing.T, value interface{}, teststr string, expSuccess bool) {
+	t.Helper()
 
-func testStuff(t *testing.T, foo interface{}, teststr string, success bool) {
 	var mb []byte
 	var mb2 interface{}
 	var err error = nil
 	var err2 error = nil
 
-	switch foo.(type) {
-		case *TCPAddr:
-			a := foo.(*TCPAddr)
-			mb, err = a.MarshalJSON()
-			mb2, err2 = a.MarshalYAML()
-		case *UDPAddr:
-			a := foo.(*UDPAddr)
-			mb, err = a.MarshalJSON()
-			mb2, err2 = a.MarshalYAML()
-		case *Addr:
-			a := foo.(*Addr)
-			mb, err = a.MarshalJSON()
-			mb2, err2 = a.MarshalYAML()
-		case *Duration:
-			d := foo.(*Duration)
-			mb, err = d.MarshalJSON()
-			mb2, err2 = d.MarshalYAML()
-		case *String:
-			s := foo.(*String)
-			mb, err = s.MarshalJSON()
-			mb2, err2 = s.MarshalYAML()
-		case *URL:
-			u := foo.(*URL)
-			mb, err = u.MarshalJSON()
-			mb2, err2 = u.MarshalYAML()
-		default:
-			t.Errorf("Test function received unexpected type: %T", foo)
-			return
+	switch value.(type) {
+	case *TCPAddr:
+		a := value.(*TCPAddr)
+		mb, err = a.MarshalJSON()
+		mb2, err2 = a.MarshalYAML()
+	case *UDPAddr:
+		a := value.(*UDPAddr)
+		mb, err = a.MarshalJSON()
+		mb2, err2 = a.MarshalYAML()
+	case *Addr:
+		a := value.(*Addr)
+		mb, err = a.MarshalJSON()
+		mb2, err2 = a.MarshalYAML()
+	case *Duration:
+		d := value.(*Duration)
+		mb, err = d.MarshalJSON()
+		mb2, err2 = d.MarshalYAML()
+	case *String:
+		s := value.(*String)
+		mb, err = s.MarshalJSON()
+		mb2, err2 = s.MarshalYAML()
+	case *URL:
+		u := value.(*URL)
+		mb, err = u.MarshalJSON()
+		mb2, err2 = u.MarshalYAML()
+	default:
+		t.Errorf("Test function received unexpected type: %T", value)
+		return
 	}
 
 	jstr := fmt.Sprintf("\"%s\"", teststr)
 
-	if !success {
-		if (err == nil && string(mb) == jstr) {
-			t.Errorf("Unexpected successful marshaling of bad %T in JSON form", foo)
+	if !expSuccess {
+		if err == nil && string(mb) == jstr {
+			t.Errorf("Unexpected successful marshaling of bad %T in JSON form", value)
 		}
 	} else if err != nil {
-		t.Errorf("Error marshaling %T in JSON form: %v", foo, err)
+		t.Errorf("Error marshaling %T in JSON form: %v", value, err)
 	} else if string(mb) != jstr {
-		t.Errorf("Marshaled JSON %T was not expected value: %v", foo, string(mb))
+		t.Errorf("Marshaled JSON %T was not expected value: %v", value, string(mb))
 	}
 
-	if !success {
+	if !expSuccess {
 		if err2 == nil {
-			if ystr, ok := mb2.(string); (ok != true || teststr == ystr) {
-				t.Errorf("Unexpected successful marshaling of bad %T in YAML form", foo)
+			if ystr, ok := mb2.(string); ok != true || teststr == ystr {
+				t.Errorf("Unexpected successful marshaling of bad %T in YAML form", value)
 			}
 		}
 	} else if err2 != nil {
-		t.Errorf("Error marshaling %T in YAML form: %v", foo, err2)
-	} else if ystr, ok := mb2.(string); (ok != true || teststr != ystr) {
-		t.Errorf("Marshaled YAML %T was not expected value: %v", foo, ystr)
+		t.Errorf("Error marshaling %T in YAML form: %v", value, err2)
+	} else if ystr, ok := mb2.(string); ok != true || teststr != ystr {
+		t.Errorf("Marshaled YAML %T was not expected value: %v", value, ystr)
 	}
 
-	if success {
-		var uerr error = nil
-		var uerr2 error = nil
+	if expSuccess {
+		var err error = nil
+		var err2 error = nil
 
 		badjson := []byte("BAD, JSON, STRING")
 
-		switch foo.(type) {
-			case *TCPAddr:
-			case *UDPAddr:
-			case *Addr:
-				a := &Addr{}
-				uerr = a.UnmarshalJSON(mb)
-				uerr2 = a.UnmarshalJSON(badjson)
-			case *Duration:
-				d := &Duration{}
-				uerr = d.UnmarshalJSON(mb)
-				uerr2 = d.UnmarshalJSON(badjson)
-			case *String:
-				s := &String{}
-				uerr = s.UnmarshalJSON(mb)
-				uerr2 = s.UnmarshalJSON(badjson)
-			case *URL:
-				u := &URL{}
-				uerr = u.UnmarshalJSON(mb)
-				uerr2 = u.UnmarshalJSON(badjson)
+		switch value.(type) {
+		case *TCPAddr:
+			a := &TCPAddr{}
+			err = a.UnmarshalJSON(mb)
+			err2 = a.UnmarshalJSON(badjson)
+		case *UDPAddr:
+			a := &UDPAddr{}
+			err = a.UnmarshalJSON(mb)
+			err2 = a.UnmarshalJSON(badjson)
+		case *Addr:
+			a := &Addr{}
+			err = a.UnmarshalJSON(mb)
+			err2 = a.UnmarshalJSON(badjson)
+		case *Duration:
+			d := &Duration{}
+			err = d.UnmarshalJSON(mb)
+			err2 = d.UnmarshalJSON(badjson)
+		case *String:
+			s := &String{}
+			err = s.UnmarshalJSON(mb)
+			err2 = s.UnmarshalJSON(badjson)
+		case *URL:
+			u := &URL{}
+			err = u.UnmarshalJSON(mb)
+			err2 = u.UnmarshalJSON(badjson)
 		}
 
-		if uerr != nil {
-			t.Errorf("Unmarshaling of JSON data into type %T produced error: %v", foo, uerr)
+		if err != nil {
+			t.Errorf("Unmarshaling of JSON data into type %T produced error: %v", value, err)
 		}
-
-		if uerr2 == nil {
-			t.Errorf("Unmarshaling of bad JSON data into type %T failed to produce error", foo)
+		if err2 == nil {
+			t.Errorf("Unmarshaling of bad JSON data into type %T failed to produce error", value)
 		}
-
 	}
-
 }
 
 func TestBadAddr(t *testing.T) {
-	ustr := "udp:1.2.3.4"
-	a := &Addr{}
-	a.Set(ustr)
-	testStuff(t, a, ustr, true)
-
-	u := &UDPAddr{}
-	ta := &TCPAddr{}
-	ua := &UnixAddr{}
-
-	for i, _ := range []int{0, 1, 2, 3} {
-		var err, err2 error = nil, nil
-
-		switch(i) {
-			case 0:
-				err = a.Set("BADADDR")
-				err2 = err
-			case 1:
-				err = u.Set("tcp:1.2.3.4")
-				err2 = u.Set("BADADDR")
-			case 2:
-				err = ta.Set("udp:1.2.3.4")
-				err2 = ta.Set("BADADDR")
-			case 3:
-				err = ua.Set("udp:1.2.3.4")
-				err2 = ua.Set("BADADDR")
+	t.Run("Addr/valid", func(t *testing.T) {
+		a := &Addr{}
+		if err := a.Set("udp:1.2.3.4"); err != nil {
+			t.Fatalf("unexpected error: %v", err)
 		}
+		testMarshalling(t, a, "udp:1.2.3.4", true)
+	})
 
-		if err == nil {
-			t.Errorf("Unexpected error-free setting of address from bad data / %d", i)
-		} else {
-			fmt.Printf("Bad address produced error: %T / %v\n", err, err)
-		}
-
-		if err2 == nil {
-			t.Errorf("Expected set of bad address to produce error but it did not")
-		}
-
+	cases := []struct {
+		name string
+		set  func(string) error
+		in   string
+	}{
+		{
+			"Addr/malformed",
+			func(s string) error { return (&Addr{}).Set(s) },
+			"BADADDR",
+		},
+		{
+			"UDPAddr/wrong_network",
+			func(s string) error { return (&UDPAddr{}).Set(s) },
+			"tcp:1.2.3.4",
+		},
+		{
+			"UDPAddr/malformed",
+			func(s string) error { return (&UDPAddr{}).Set(s) },
+			"BADADDR",
+		},
+		{
+			"TCPAddr/wrong_network",
+			func(s string) error { return (&TCPAddr{}).Set(s) },
+			"udp:1.2.3.4",
+		},
+		{
+			"TCPAddr/malformed",
+			func(s string) error { return (&TCPAddr{}).Set(s) },
+			"BADADDR",
+		},
+		{
+			"UnixAddr/wrong_network",
+			func(s string) error { return (&UnixAddr{}).Set(s) },
+			"udp:1.2.3.4",
+		},
+		{
+			"UnixAddr/malformed",
+			func(s string) error { return (&UnixAddr{}).Set(s) },
+			"BADADDR",
+		},
 	}
-
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			if err := tc.set(tc.in); err == nil {
+				t.Errorf("expected error for input %q", tc.in)
+			}
+		})
+	}
 }
 
 func TestDuration(t *testing.T) {
 	ustr := "100ms"
 	d := &Duration{}
 	d.Set(ustr)
-	testStuff(t, d, ustr, true)
+	testMarshalling(t, d, ustr, true)
 
 	ustr = "1m30duif"
 	d.Set(ustr)
-	testStuff(t, d, ustr, false)
+	testMarshalling(t, d, ustr, false)
 }
 
 func TestString(t *testing.T) {
@@ -191,7 +221,7 @@ func TestString(t *testing.T) {
 		t.Errorf("Unexpected string set error")
 	}
 
-	testStuff(t, s, ustr, true)
+	testMarshalling(t, s, ustr, true)
 
 	envname := "ENV_DOES_NOT_EXIST"
 	envval := "abc123"
@@ -222,7 +252,6 @@ func TestString(t *testing.T) {
 		t.Errorf("Failed to get tmpfile for String test")
 	}
 
-
 }
 
 func TestURL(t *testing.T) {
@@ -230,14 +259,13 @@ func TestURL(t *testing.T) {
 	u := &URL{}
 	u.Set(ustr)
 
-	testStuff(t, u, ustr, true)
+	testMarshalling(t, u, ustr, true)
 }
 
 func TestFile(t *testing.T) {
 	var dummystr interface{}
 
 	if dne_path, err := os.Getwd(); err == nil {
-
 		dne_path += "/doooooooooes_nooooooot_exist"
 
 		if err := LoadJSON(&dummystr, dne_path, true); err == nil {
@@ -284,20 +312,18 @@ func TestFile(t *testing.T) {
 	} else {
 		t.Errorf("Failed to get YAML tmpfile")
 	}
-
 }
 
-
 type ExConf struct {
-	Str1		String
-	EnvString	String
-	Num1		uint64
-	Url		URL
-	Dur		Duration
-	Addr		Addr
-	UAddr		UnixAddr
-	TAddr		TCPAddr
-	UDAddr		UDPAddr
+	Str1      String
+	EnvString String
+	Num1      uint64
+	Url       URL
+	Dur       Duration
+	Addr      Addr
+	UAddr     UnixAddr
+	TAddr     TCPAddr
+	UDAddr    UDPAddr
 }
 
 func TestFile2(t *testing.T) {
@@ -317,8 +343,9 @@ func TestFile2(t *testing.T) {
 		t.Errorf("Failed to get JSON tmpfile")
 	}
 
-	b, e := json.Marshal(dummyconf1)
-	fmt.Printf("Its: %v, %v\n", string(b), e)
+	if _, err := json.Marshal(dummyconf1); err != nil {
+		t.Errorf("Failed to marshal JSON config: %v", err)
+	}
 
 	if tmpy := getTempFile(t, "yml", ydata); tmpy != "" {
 		defer os.Remove(tmpy)
@@ -326,17 +353,18 @@ func TestFile2(t *testing.T) {
 		if err := LoadYAML(&dummyconf2, tmpy, true); err != nil {
 			t.Errorf("Error loading dummy YAML config: %v", err)
 		}
-
 	} else {
 		t.Errorf("Failed to get YAML tmpfile")
 	}
 
-	b, e = yaml.Marshal(dummyconf2)
+	if _, err := yaml.Marshal(dummyconf2); err != nil {
+		t.Errorf("Failed to marshal YAML config: %v", err)
+	}
 }
 
 type TLSConf struct {
-	Conf		TLS
-	Cauth		TLSClientAuth
+	Conf  TLS
+	Cauth TLSClientAuth
 }
 
 func TestTLS(t *testing.T) {
@@ -345,16 +373,12 @@ func TestTLS(t *testing.T) {
 
 	if err := ta.Set(ustr); err == nil {
 		t.Errorf("Expected Bad TLS client auth type to produce error")
-	} else {
-		fmt.Printf("Bad TLS client auth produced error: %v\n", err)
 	}
 
 	ustr = "require"
 	if err := ta.Set(ustr); err != nil {
 		t.Errorf("Error in setting TLSClientAuth: %v", err)
 	}
-
-	fmt.Printf("Client auth val: %s\n", ta.String())
 
 	jdata := `{ "cauth": "require" }`
 	ydata := "cauth: require"
@@ -367,8 +391,9 @@ func TestTLS(t *testing.T) {
 			t.Errorf("Error loading dummy JSON config: %v", err)
 		}
 
-		b, e := json.Marshal(testconf)
-		fmt.Printf("TLSClientAuth JSON marshaling: %v, %v\n", string(b), e)
+		if _, err := json.Marshal(testconf); err != nil {
+			t.Errorf("TLSClientAuth JSON marshaling error: %v", err)
+		}
 	} else {
 		t.Errorf("Failed to get JSON tmpfile")
 	}
@@ -381,8 +406,9 @@ func TestTLS(t *testing.T) {
 			t.Errorf("Error loading dummy YAML config: %v", err)
 		}
 
-		b, e := yaml.Marshal(testconf)
-		fmt.Printf("TLSClientAuth YAML marshaling: %v, %v\n", string(b), e)
+		if _, err := yaml.Marshal(testconf); err != nil {
+			t.Errorf("TLSClientAuth YAML marshaling error: %v", err)
+		}
 	} else {
 		t.Errorf("Failed to get YAML tmpfile")
 	}
@@ -398,8 +424,9 @@ func TestTLS(t *testing.T) {
 			t.Errorf("Error loading dummy JSON config: %v", err)
 		}
 
-		b, e := json.Marshal(testconf)
-		fmt.Printf("TLS JSON marshaling: %v, %v\n", string(b), e)
+		if _, err := json.Marshal(testconf); err != nil {
+			t.Errorf("TLS JSON marshaling error: %v", err)
+		}
 	} else {
 		t.Errorf("Failed to get JSON tmpfile")
 	}
@@ -412,8 +439,9 @@ func TestTLS(t *testing.T) {
 			t.Errorf("Error loading dummy YAML config: %v", err)
 		}
 
-		b, e := yaml.Marshal(testconf)
-		fmt.Printf("TLS YAML marshaling: %v, %v\n", string(b), e)
+		if _, err := yaml.Marshal(testconf); err != nil {
+			t.Errorf("TLS YAML marshaling error: %v", err)
+		}
 	} else {
 		t.Errorf("Failed to get YAML tmpfile")
 	}
@@ -425,7 +453,6 @@ func TestTLS(t *testing.T) {
 	}
 
 	for _, i := range badconfs {
-
 		if tmpj := getTempFile(t, "json", i); tmpj != "" {
 			var testconf TLSConf
 			defer os.Remove(tmpj)
@@ -437,8 +464,117 @@ func TestTLS(t *testing.T) {
 		} else {
 			t.Errorf("Failed to get JSON tmpfile")
 		}
+	}
+}
 
+func TestUnixAddr(t *testing.T) {
+	u := &UnixAddr{}
+	if err := u.Set("unix:/tmp/sock"); err != nil {
+		t.Errorf("Error setting UnixAddr: %v", err)
 	}
 
+	mb, err := u.MarshalJSON()
+	if err != nil {
+		t.Errorf("UnixAddr MarshalJSON error: %v", err)
+	}
+	if string(mb) != `"unix:/tmp/sock"` {
+		t.Errorf("UnixAddr MarshalJSON unexpected value: %s", string(mb))
+	}
 
+	my, err := u.MarshalYAML()
+	if err != nil {
+		t.Errorf("UnixAddr MarshalYAML error: %v", err)
+	}
+	if my != "unix:/tmp/sock" {
+		t.Errorf("UnixAddr MarshalYAML unexpected value: %v", my)
+	}
+
+	u2 := &UnixAddr{}
+	if err := u2.UnmarshalJSON(mb); err != nil {
+		t.Errorf("UnixAddr UnmarshalJSON error: %v", err)
+	}
+	if u2.String() != u.String() {
+		t.Errorf("UnixAddr UnmarshalJSON value mismatch: %s", u2.String())
+	}
+
+	if err := u2.UnmarshalJSON([]byte("BAD, JSON")); err == nil {
+		t.Errorf("Expected UnixAddr UnmarshalJSON bad input to produce error")
+	}
+}
+
+func TestStringSourcePreserved(t *testing.T) {
+	s := &String{}
+	envname := "TEST_STRING_SOURCE"
+	if err := os.Setenv(envname, "envvalue"); err != nil {
+		t.Errorf("os.Setenv error: %v", err)
+		return
+	}
+	if err := s.Set("$" + envname); err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if s.String() != "envvalue" {
+		t.Errorf("Expected expanded value, got: %s", s.String())
+	}
+	mb, err := s.MarshalJSON()
+	if err != nil {
+		t.Errorf("MarshalJSON error: %v", err)
+	}
+	if string(mb) != fmt.Sprintf(`"$%s"`, envname) {
+		t.Errorf("MarshalJSON should preserve source form, got: %s", string(mb))
+	}
+}
+
+func TestStringRelativePath(t *testing.T) {
+	tmpdata := "relative path content"
+	tmpfile, err := os.CreateTemp(".", "tmpdata.*.txt")
+	if err != nil {
+		t.Errorf("Error creating temp file: %v", err)
+		return
+	}
+	tmpfile.Close()
+	defer os.Remove(tmpfile.Name())
+
+	if err := os.WriteFile(tmpfile.Name(), []byte(tmpdata), 0600); err != nil {
+		t.Errorf("Error writing temp file: %v", err)
+		return
+	}
+
+	s := &String{}
+	if err := s.Set("./" + tmpfile.Name()); err != nil {
+		t.Errorf("Error loading string from relative path: %v", err)
+	} else if s.String() != tmpdata {
+		t.Errorf("Unexpected string value from file: %s", s.String())
+	}
+}
+
+func TestTLSClientAuthInvalidMarshal(t *testing.T) {
+	ta := TLSClientAuth{tls.ClientAuthType(99)}
+	if _, err := ta.MarshalJSON(); err == nil {
+		t.Errorf("Expected MarshalJSON of invalid TLSClientAuth to produce error")
+	}
+	if _, err := ta.MarshalYAML(); err == nil {
+		t.Errorf("Expected MarshalYAML of invalid TLSClientAuth to produce error")
+	}
+}
+
+func TestFileMalformed(t *testing.T) {
+	var dummy interface{}
+
+	if tmpj := getTempFile(t, "json", "NOT { valid JSON"); tmpj != "" {
+		defer os.Remove(tmpj)
+		if err := LoadJSON(&dummy, tmpj, true); err == nil {
+			t.Errorf("Expected malformed JSON file to produce error")
+		}
+	} else {
+		t.Errorf("Failed to get JSON tmpfile")
+	}
+
+	if tmpy := getTempFile(t, "yml", "key: [unclosed"); tmpy != "" {
+		defer os.Remove(tmpy)
+		if err := LoadYAML(&dummy, tmpy, true); err == nil {
+			t.Errorf("Expected malformed YAML file to produce error")
+		}
+	} else {
+		t.Errorf("Failed to get YAML tmpfile")
+	}
 }
